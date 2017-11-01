b8bc3193c564158895e5b147a18cab57ceea29ad
Hary <yo@email.com>
2017-11-01 00:01:27 -0400

    Linking form to backend

--- crimemap.py
+++ crimemap.py
@@ -24,13 +24,14 @@ def home():
         data = None
     return render_template('home.html', data = data)
 
-@app.route('/add', methods = ['POST'])
-def add():
-    try:
-        data = request.form.get('userinput')
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
+@app.route('/submitcrime', methods=['POST'])
+def submitcrime():
+    category = request.form.get('category')
+    date = request.form.ger('date')
+    latitude = float(request.form.get('latitude'))
+    longitude = float(request.form.ger('longitude'))
+    description = request.form.get('description')
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 @app.route('/clear')
--- dbhelper.py
+++ dbhelper.py
@@ -9,6 +9,22 @@ class DBHelper():
                 passwd = dbconfig.db_password,
                 db = database)
 
+    def add_crime(self, category, date, latitude, longitude, description):
+        connection = self.connect()
+
+        try:
+            query = "INSERT INTO crimes (category, date, latitude, longitude, description) VALUES (%s, %s, %s, %s, %s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (category, date, latitude, longitude, description))
+                connection.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            connection.close()
+
+
+# to be removed
+
     def get_all_inputs(self):
         connection = self.connect()
         try:
@@ -16,16 +32,6 @@ class DBHelper():
             with connection.cursor() as cursor:
                 cursor.execute(query)
             return cursor.fetchall()
-        finally:
-            connection.close()
-
-    def add_input(self, data):
-        connection = self.connect()
-        try:
-            query = "INSERT INTO crimes (description) VALUES (%s);"
-            with connection.cursor() as cursor:
-                cursor.execute(query, data)
-                connection.commit()
         finally:
             connection.close()
 
